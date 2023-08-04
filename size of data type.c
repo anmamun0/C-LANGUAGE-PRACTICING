d7dc1e8fa572a226bbb31957d1facec15fb171d9
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2023-08-05 02:03:23 +0600

    Create size of data type.c

---
+++ size of data type.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+
+
+int  inttype;
+float floattype;
+double doubletype;
+char chartype;
+
+printf("Int  size : %zu \n", sizeof(inttype));
+printf("Float Type : %zu \n", sizeof(floattype));
+printf("Double Type : %zu \n", sizeof(doubletype));
+printf("charter Type : %zu \n", sizeof(chartype));
+
+
+
+return 0;
+}
