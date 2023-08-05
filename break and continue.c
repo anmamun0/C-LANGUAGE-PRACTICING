4649e6b79c87078b73d939b0391f3ca6a9a6fd5b
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2023-08-05 16:56:52 +0600

    Create break and continue.c

---
+++ break and continue.c
@@ -0,0 +1,45 @@
+/*--------------------------------------Break---------------------------------------------*/
+#include <stdio.h>
+int main() {
+   int i;
+   double number, sum = 0.0;
+
+   for (i = 1; i <= 10; ++i) {
+      printf("Enter n%d: ", i);
+      scanf("%lf", &number);
+
+      // if the user enters a negative number, break the loop
+      if (number < 0.0) {
+         break;
+      }
+
+      sum += number; // sum = sum + number;
+   }
+
+   printf("Sum = %.2lf", sum);
+
+   return 0;
+}
+
+/*-------------------------------------Continue ------------------------------------------*/
+
+#include <stdio.h>
+int main() {
+   int i;
+   double number, sum = 0.0;
+
+   for (i = 1; i <= 10; ++i) {
+      printf("Enter a n%d: ", i);
+      scanf("%lf", &number);
+
+      if (number < 0.0) {
+         continue;
+      }
+
+      sum += number; // sum = sum + number;
+   }
+
+   printf("Sum = %.2lf", sum);
+
+   return 0;
+}
