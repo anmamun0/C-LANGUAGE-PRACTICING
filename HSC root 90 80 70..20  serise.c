e992e826e5c6906496954645f1ce511ebb5e7c08
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2023-08-25 15:06:19 +0600

    Update HSC root 90 80 70..20  serise.c

--- HSC root 90 80 70..20  serise.c
+++ HSC root 90 80 70..20  serise.c
@@ -3,6 +3,7 @@
 
 int main() 
 {
+//-----------------goto statment ---------------------------------
     int sum=0, i=90, n=20;
 
     jump:
@@ -11,5 +12,25 @@ int main()
         if (i>=n) goto jump;
     printf("%d ", sum);
 
+//---------------------while statment--------------------------
+    int sum=0, i=90, n=20;
+
+    while (i>=n)
+    {
+        sum += i*i;
+        i-=10;
+    }
+    printf("Sum: %d ", sum);  
+
+//--------------------for statment--------------------------
+        int sum= 0, i=90, n =20;
+
+    for(i; i>=n; i-=10){
+        sum += i*i;
+        printf("(%d)^2  ", i);
+
+    }
+    printf("Here is Sum; %d", sum);
+}
 
 }
